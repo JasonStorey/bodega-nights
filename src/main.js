bfa66ee18e0d873ceb2033185f4723ddb4bf1b61
Jason Storey <jason.andrew.storey@gmail.com>
2014-10-05 22:31:57 +0100

    Add crosshairs sprite and handle cursor button input

--- src/main.js
+++ src/main.js
@@ -1,19 +1,53 @@
 define(['Phaser'], function (Phaser) {
-    var game;
+    var game,
+        cursors,
+        crosshair;
 
     function init(containerElement) {
-        game = new Phaser.Game(800, 600, Phaser.AUTO, containerElement, {
+        game = new Phaser.Game('100', '100', Phaser.AUTO, containerElement, {
             preload: preload,
             create: create,
             update: update
         });
     }
 
-    function preload() {}
+    function preload() {
+        game.stage.backgroundColor = '#ffffff';
+        game.load.image('crosshair', './assets/crosshair.png');
+    }
 
-    function create() {}
+    function create() {
+        crosshair = game.add.sprite(game.width / 2, game.height / 2, 'crosshair');
+        crosshair.anchor.setTo(0.5, 0.5);
+        crosshair.scale.setTo(0.5, 0.5);
 
-    function update() {}
+        game.physics.enable(crosshair, Phaser.Physics.ARCADE);
+        crosshair.body.drag.set(0.5);
+        crosshair.body.maxVelocity.setTo(2000, 2000);
+        crosshair.body.collideWorldBounds = true;
+
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+
+    function update() {
+        var acceleration = 100;
+
+        if (cursors.left.isDown) {
+            crosshair.body.velocity.x -= acceleration;
+        } else if (cursors.right.isDown) {
+            crosshair.body.velocity.x += acceleration;
+        } else {
+            crosshair.body.velocity.x = crosshair.body.velocity.x * 0.9;
+        }
+
+        if (cursors.up.isDown) {
+            crosshair.body.velocity.y -= acceleration;
+        } else if (cursors.down.isDown) {
+            crosshair.body.velocity.y += acceleration;
+        } else {
+            crosshair.body.velocity.y = crosshair.body.velocity.y * 0.9;
+        }
+    }
 
     return {
         init: init
